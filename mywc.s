833bef098240be5305185791e39a8d9b5c231088
Anders Oja <ao6499@princeton.edu>
2024-04-21 23:18:36 -0400

    first .s attempt

--- mywc.s
+++ mywc.s
@@ -54,47 +54,90 @@ main:
 
 startWhile:
         // if ((iChar = getchar()) == EOF) goto endWhile;
+        bl      getchar
+        adr     x1, iChar
+        str     x0, [x1]
+        ldr     x0, [x1]
+        mov     x1, EOF
+        cmp     x0, w1
+        beq     endWhile
 
-
-powerLoop:
-
-        // if (lIndex > lExp) goto powerLoopEnd
-        adr     x0, lIndex
-        ldr     x0, [x0]
-        adr     x1, lExp
-        ldr     x1, [x1]
-        cmp     x0, x1
-        bgt     powerLoopEnd
-
-        // lPower *= lBase
-        adr     x0, lPower
-        ldr     x1, [x0]
-        adr     x2, lBase
-        ldr     x2, [x2]
-        mul     x1, x1, x2
-        str     x1, [x0]
-
-        // lIndex++
-        adr     x0, lIndex
+        //lCharCount++
+        adr     x0, lCharCount
         ldr     x1, [x0]
         add     x1, x1, 1
         str     x1, [x0]
 
-        // goto powerLoop
-        b       powerLoop
+        //if (!isspace(iChar)) goto Else;
+        adr     x0, iChar
+        ldr     x0, [x0]
+        bl      isspace
+        cmp     x0, FALSE
+        beq     Else
 
-powerLoopEnd:
+        //if (!iInWord) goto endIf1;
+        adr     x0, iInWord
+        ldr     x0, [x0]
+        cmp     x0, FALSE
+        beq     endIf1
+        //lWordCount++;
+        adr     x0, lWordCount
+        ldr     x1, [x0]
+        add     x1, x1, 1
+        str     x1, [x0]
+        //iInWord = FALSE;
+        adr     x0, iInWord
+        mov     x1, FALSE
+        str     x1, [x0]
+        //goto endIf1;
+        b       endIf1
+Else:
+        //if (iInWord) goto endIf1;
+        adr     x0, iInWord
+        ldr     x0, [x0]
+        cmp     x0, TRUE
+        beq     endIf1
+        //iInWord = TRUE;
+        adr     x0, iInWord
+        mov     x1, TRUE
+        str     x1, [x0]
+endIf1:
+        //if (iChar != '\n') goto startWhile;
+        adr     x0, iChar
+        ldr     x0, [x0]
+        mov     x1, '\n'
+        cmp     x0, x1
+        bne    startWhile
+        //lLineCount++;
+        adr     x0, lLineCount
+        ldr     x1, [x0]
+        add     x1, x1, 1
+        str     x1, [x0]
+        // goto startWhile;
+        b       startWhile
 
-        // printf("%ld to the %ld power is %ld.\n", lBase, lExp, lPower)
+endWhile:
+        //if (!iInWord) goto endIf2;
+        adr     x0, iInWord
+        ldr     x0, [x0]
+        cmp     x0, FALSE
+        beq     endIf2
+        //lWordCount++;
+        adr     x0, lWordCount
+        ldr     x1, [x0]
+        add     x1, x1, 1
+        str     x1, [x0]
+endIf2:
+        //printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
         adr     x0, printfFormatStr
-        adr     x1, lBase
+        adr     x1, lLineCount
         ldr     x1, [x1]
-        adr     x2, lExp
+        adr     x2, lWordCount
         ldr     x2, [x2]
-        adr     x3, lPower
+        adr     x3, lCharCount
         ldr     x3, [x3]
         bl      printf
-        
+
         // Epilog and return 0
         mov     w0, 0
         ldr     x30, [sp]
