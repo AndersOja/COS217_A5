17a56848f4853c18723ee97f0be4df7c3b2d7337
Anders Oja <ao6499@princeton.edu>
2024-04-22 21:34:23 -0400

    a;lds

---
+++ bigintadd.s
@@ -0,0 +1,151 @@
+        .equ    FALSE, 0
+        .equ    TRUE, 1
+
+//----------------------------------------------------------------------  
+
+        .section .rodata
+
+//----------------------------------------------------------------------
+
+        .section .data
+        
+
+//----------------------------------------------------------------------
+
+        .section .bss
+
+
+//----------------------------------------------------------------------
+
+        .section .text
+
+        //--------------------------------------------------------------
+        // Write to stdout counts of how many lines, words, and 
+        // characters are in stdin. A word is a sequence of 
+        // non-whitespace characters. Whitespace is defined by the 
+        // isspace() function. Return 0. 
+        // int main(void)
+        //--------------------------------------------------------------
+
+        // Must be a multiple of 16
+        .equ    LARGER_STACK_BYTECOUNT, 32
+        .equ    ADD_STACK_BYTECOUNT, 64
+
+        // Offsets for local variables
+        .equ    lLength1, 8
+        .equ    lLength2, 16
+        .equ    lLarger, 24
+
+        .global BigInt_add
+        .private BigInt_larger
+
+BigInt_larger:
+        // Prolog
+        sub     sp, sp, LARGER_STACK_BYTECOUNT
+        str     x30, [sp]
+        ldr     x0, [sp + lLength1]
+
+        // if (lLength1 <= lLength2) goto Else1;
+        
+
+startWhile:
+        // if ((iChar = getchar()) == EOF) goto endWhile;
+        bl      getchar
+        adr     x1, iChar
+        strb    w0, [x1]
+        cmp     w0, EOF
+        beq     endWhile
+
+        // lCharCount++
+        adr     x0, lCharCount
+        ldr     x1, [x0]
+        add     x1, x1, 1
+        str     x1, [x0]
+
+        // if (!isspace(iChar)) goto Else
+        adr     x0, iChar
+        ldrb    w0, [x0]
+        bl      isspace
+        cmp     x0, FALSE
+        beq     Else
+
+        // if (!iInWord) goto endIf1
+        adr     x0, iInWord
+        ldr     x0, [x0]
+        cmp     x0, FALSE
+        beq     endIf1
+
+        // lWordCount++
+        adr     x0, lWordCount
+        ldr     x1, [x0]
+        add     x1, x1, 1
+        str     x1, [x0]
+
+        // iInWord = FALSE
+        adr     x0, iInWord
+        mov     x1, FALSE
+        str     x1, [x0]
+
+        // goto endIf1
+        b       endIf1
+
+Else:
+        // if (iInWord) goto endIf1
+        adr     x0, iInWord
+        ldr     x0, [x0]
+        cmp     x0, TRUE
+        beq     endIf1
+
+        // iInWord = TRUE
+        adr     x0, iInWord
+        mov     x1, TRUE
+        str     x1, [x0]
+
+endIf1:
+        // if (iChar != '\n') goto startWhile
+        adr     x0, iChar
+        ldrb     w0, [x0]
+        mov     x1, '\n'
+        cmp     x0, x1
+        bne    startWhile
+
+        // lLineCount++
+        adr     x0, lLineCount
+        ldr     x1, [x0]
+        add     x1, x1, 1
+        str     x1, [x0]
+        
+        // goto startWhile
+        b       startWhile
+
+endWhile:
+        // if (!iInWord) goto endIf2
+        adr     x0, iInWord
+        ldr     x0, [x0]
+        cmp     x0, FALSE
+        beq     endIf2
+        // lWordCount++
+        adr     x0, lWordCount
+        ldr     x1, [x0]
+        add     x1, x1, 1
+        str     x1, [x0]
+
+endIf2:
+        // printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount)
+        adr     x0, printfFormatStr
+        adr     x1, lLineCount
+        ldr     x1, [x1]
+        adr     x2, lWordCount
+        ldr     x2, [x2]
+        adr     x3, lCharCount
+        ldr     x3, [x3]
+        bl      printf
+
+        // Epilog and return 0
+        mov     w0, 0
+        ldr     x30, [sp]
+        add     sp, sp, MAIN_STACK_BYTECOUNT
+        ret
+
+        .size   main, (. - main)
+        
