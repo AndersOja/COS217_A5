8562932e52418b36f05596a56cd73a03cc9cb444
Joey Forzani <jf4555@princeton.edu>
2024-04-23 14:03:33 -0400

    bug fixes in add.s

--- bigintadd.s
+++ bigintadd.s
@@ -9,23 +9,17 @@
 //----------------------------------------------------------------------
 
         .section .data
-        
 
 //----------------------------------------------------------------------
 
         .section .bss
-
 
 //----------------------------------------------------------------------
 
         .section .text
 
         //--------------------------------------------------------------
-        // Write to stdout counts of how many lines, words, and 
-        // characters are in stdin. A word is a sequence of 
-        // non-whitespace characters. Whitespace is defined by the 
-        // isspace() function. Return 0. 
-        // int main(void)
+        // Return the larger of lLength1 and lLength2.
         //--------------------------------------------------------------
 
         // Must be a multiple of 16
@@ -41,21 +35,37 @@ BigInt_larger:
         // Prolog
         sub     sp, sp, LARGER_STACK_BYTECOUNT
         str     x30, [sp]
-        // if (lLength1 <= lLength2) goto Else1;
+        str     x0, [sp, lLength1]
+        str     x1, [sp, lLength2]
+
+        // if (lLength1 <= lLength2) goto Else1
         cmp     x0, x1
         ble     Else1
-        // lLarger = lLength1;
+
+        // lLarger = lLength1
+        ldr     x0, [sp, lLength1]
         str     x0, [sp, lLarger]
+
         // goto endIf1
         b       endIf1
+
 Else1:
         ldr     x0, [sp, lLength2]
         str     x0, [sp, lLarger]
+
 endIf1:
+        ldr     x0, [sp, lLarger]
+        ldr     x30, [sp]
+        add     sp, sp, LARGER_STACK_BYTECOUNT
         ret
 
         .size   BigInt_larger, (. - BigInt_larger)
 
+        //--------------------------------------------------------------
+        // Assign the sum of oAddend1 and oAddend2 to oSum. oSum should 
+        // be distinct from oAddend1 and oAddend2.  Return 0 (FALSE) 
+        // if an overflow occurred, and 1 (TRUE) otherwise.
+        //--------------------------------------------------------------
 
         .equ    ADD_STACK_BYTECOUNT, 64
 
@@ -74,22 +84,27 @@ BigInt_add:
         // Prolog
         sub     sp, sp, ADD_STACK_BYTECOUNT
         str     x30, [sp]
+        str     x0, [sp, oAddend1]
+        str     x1, [sp, oAddend2]
+        str     x2, [sp, oSum]
 
-        //lSumLength = BigInt_larger(oAddend1->lLength, 
-        // oAddend2->lLength);
+        // lSumLength = BigInt_larger(oAddend1->lLength, 
+        // oAddend2->lLength)
         ldr     x0, [sp, oAddend1]
         ldr     x0, [x0]
         ldr     x1, [sp, oAddend2]
         ldr     x1, [x1]
         bl      BigInt_larger
         str     x0, [sp, lSumLength]
-        // if (oSum->lLength <= lSumLength) goto endIf2;
+
+        // if (oSum->lLength <= lSumLength) goto endIf2
         ldr     x0, [sp, oSum]
         ldr     x0, [x0]
         ldr     x1, [sp, lSumLength]
         cmp     x0, x1
         ble     endIf2
-        // memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
+
+        // memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long))
         ldr     x0, [sp, oSum]
         add     x0, x0, 8
         ldr     x0, [x0]
@@ -98,25 +113,31 @@ BigInt_add:
         mov     x3, 8
         mul     x2, x2, x3
         bl      memset
+
 endIf2:
-        // ulCarry = 0;
+        // ulCarry = 0
         mov x0, 0
         str     x0, [sp, ulCarry]
-        // lIndex = 0;
+
+        // lIndex = 0
         str     x0, [sp, lIndex]
+
 startForLoop1:
-        // if(lIndex >= lSumLength) goto endForLoop1;
+        // if(lIndex >= lSumLength) goto endForLoop1
         ldr     x0, [sp, lIndex]
         ldr     x1, [sp, lSumLength]
         cmp     x0, x1
         bge     endForLoop1
-        // ulSum = ulCarry;
+
+        // ulSum = ulCarry
         ldr     x0, [sp, ulCarry]
         str     x0, [sp, ulSum]
-        // ulCarry = 0;
+
+        // ulCarry = 0
         mov     x0, 0
         str     x0, [sp, ulCarry]
-        // ulSum += oAddend1->aulDigits[lIndex];
+        
+        // ulSum += oAddend1->aulDigits[lIndex]
         ldr     x0, [sp, ulSum]
         ldr     x1, [sp, oAddend1]
         add     x1, x1, 8
@@ -124,7 +145,8 @@ startForLoop1:
         ldr     x1, [x1, x2, lsl 3]
         add     x0, x0, x1
         str     x0, [sp, ulSum]
-        // if (ulSum >= oAddend1->aulDigits[lIndex]) goto ForIf1;
+
+        // if (ulSum >= oAddend1->aulDigits[lIndex]) goto ForIf1
         ldr     x0, [sp, ulSum]
         ldr     x1, [sp, oAddend1]
         add     x1, x1, 8
@@ -132,18 +154,21 @@ startForLoop1:
         ldr     x1, [x1, x2, lsl 3]
         cmp     x0, x1
         bge     ForIf1
+
         // ulCarry = 1
         mov     x0, 1
         str     x0, [sp, ulCarry]
+
 ForIf1:
-        // ulSum += oAddend2->aulDigits[lIndex];
+        // ulSum += oAddend2->aulDigits[lIndex]
         ldr     x0, [sp, ulSum]
         ldr     x1, [sp, oAddend2]
         add     x1, x1, 8
         ldr     x2, [sp, lIndex]
         ldr     x1, [x1, x2, lsl 3]
         add     x0, x0, x1
-        // if (ulSum >= oAddend2->aulDigits[lIndex]) goto ForIf2;
+
+        // if (ulSum >= oAddend2->aulDigits[lIndex]) goto ForIf2
         ldr     x0, [sp, ulSum]
         ldr     x1, [sp, oAddend2]
         add     x1, x1, 8
@@ -151,51 +176,70 @@ ForIf1:
         ldr     x1, [x1, x2, lsl 3]
         cmp     x0, x1
         bge     ForIf2
-        // ulCarry = 1;
+
+        // ulCarry = 1
         mov     x0, 1
         str     x0, [sp, ulCarry]
+
     ForIf2:
-        // oSum->aulDigits[lIndex] = ulSum;
+        // oSum->aulDigits[lIndex] = ulSum
         ldr     x0, [sp, ulSum]
         ldr     x1, [sp, oSum]
         add     x1, x1, 8
         ldr     x2, [sp, lIndex]
         str     x0, [x1, x2, lsl 3]
-        // lIndex++;
-        add     x2, x2, 1
-        str     x2, [sp, lIndex]
+
+        // lIndex++
+        ldr     x0, [sp, lIndex]
+        add     x0, x0, 1
+        str     x0, [sp, lIndex]
+
         // goto startForLoop1
         b       startForLoop1
+
 endForLoop1:
-        // if (ulCarry != 1) goto endIf3;
+        // if (ulCarry != 1) goto endIf3
         ldr     x0, [sp, ulCarry]
         mov     x1, 1
         cmp     x0, x1
         bne     endIf3
-        // if (lSumLength != MAX_DIGITS) goto endIf4;
+
+        // if (lSumLength != MAX_DIGITS) goto endIf4
         ldr     x0, [sp, lSumLength]
         mov     x1, MAX_DIGITS
         cmp     x0, x1
         bne     endIf4
-        // return FALSE;
+
+        // Epilogue and return FALSE
         mov     x0, FALSE
+        ldr     x30, [sp]
+        add     sp, sp, ADD_STACK_BYTECOUNT
         ret
+
 endIf4:
-        // oSum->aulDigits[lSumLength] = 1;
+        // oSum->aulDigits[lSumLength] = 1
         mov     x0, 1
         ldr     x1, [sp, oSum]
         add     x1, x1, 8
         ldr     x2, [sp, lSumLength]
         str     x0, [x1, x2, lsl 3]
-        // lSumLength++;
-        add     x2, x2, 1
+
+        // lSumLength++
+        ldr     x0, [sp, lSumLength]
+        add     x0, x0, 1
+        str     x0, [sp, lSumLength]
+
 endIf3:
-        //oSum->lLength = lSumLength;
+        // oSum->lLength = lSumLength
         ldr     x0, [sp, lSumLength]
         ldr     x1, [sp, oSum]
+        ldr     x1, [x1]
         str     x0, [x1]
-        //return TRUE;
+
+        // Epilogue and return TRUE
         mov x0, TRUE
+        ldr     x30, [sp]
+        add     sp, sp, ADD_STACK_BYTECOUNT
         ret  
 
         .size   BigInt_add, (. - BigInt_add)
--- bigintaddflattened.c
+++ bigintaddflattened.c
@@ -18,12 +18,13 @@ enum {FALSE, TRUE};
 static long BigInt_larger(long lLength1, long lLength2)
 {
    long lLarger;
-   if (lLength1 <= lLength2) goto Else1;
-      lLarger = lLength1;
-      goto endIf1;
-   Else1:
-      lLarger = lLength2;
-    endIf1:
+   if (lLength1 <= lLength2)
+      goto Else1;
+   lLarger = lLength1;
+   goto endIf1;
+Else1:
+   lLarger = lLength2;
+endIf1:
    return lLarger;
 }
 
@@ -44,40 +45,46 @@ int BigInt_add(BigInt_T oAddend1, BigInt_T oAddend2, BigInt_T oSum)
    lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength);
 
    /* Clear oSum's array if necessary. */
-   if (oSum->lLength <= lSumLength) goto endIf2;
-      memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
-   endIf2:
+   if (oSum->lLength <= lSumLength)
+      goto endIf2;
+   memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
+endIf2:
    /* Perform the addition. */
    ulCarry = 0;
    lIndex = 0;
-   startForLoop1:
-   if(lIndex >= lSumLength) goto endForLoop1;
-      ulSum = ulCarry;
-      ulCarry = 0;
+startForLoop1:
+   if (lIndex >= lSumLength)
+      goto endForLoop1;
+   ulSum = ulCarry;
+   ulCarry = 0;
 
-      ulSum += oAddend1->aulDigits[lIndex];
-      if (ulSum >= oAddend1->aulDigits[lIndex]) goto ForIf1;/* Check for overflow. */
-         ulCarry = 1;
-      ForIf1:
+   ulSum += oAddend1->aulDigits[lIndex];
+   if (ulSum >= oAddend1->aulDigits[lIndex])
+      goto ForIf1; /* Check for overflow. */
+   ulCarry = 1;
+ForIf1:
 
-      ulSum += oAddend2->aulDigits[lIndex];
-      if (ulSum >= oAddend2->aulDigits[lIndex]) goto ForIf2;/* Check for overflow. */
-         ulCarry = 1;
-      ForIf2:
+   ulSum += oAddend2->aulDigits[lIndex];
+   if (ulSum >= oAddend2->aulDigits[lIndex])
+      goto ForIf2; /* Check for overflow. */
+   ulCarry = 1;
+ForIf2:
 
-      oSum->aulDigits[lIndex] = ulSum;
-      lIndex++;
-      goto startForLoop1;
-   endForLoop1:
+   oSum->aulDigits[lIndex] = ulSum;
+   lIndex++;
+   goto startForLoop1;
+endForLoop1:
 
    /* Check for a carry out of the last "column" of the addition. */
-   if (ulCarry != 1) goto endIf3;
-      if (lSumLength != MAX_DIGITS) goto endIf4;
-         return FALSE;
-      endIf4:
-      oSum->aulDigits[lSumLength] = 1;
-      lSumLength++;
-   endIf3:
+   if (ulCarry != 1)
+      goto endIf3;
+   if (lSumLength != MAX_DIGITS)
+      goto endIf4;
+   return FALSE;
+endIf4:
+   oSum->aulDigits[lSumLength] = 1;
+   lSumLength++;
+endIf3:
 
    /* Set the length of the sum. */
    oSum->lLength = lSumLength;
