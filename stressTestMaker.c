f008103414b1b74d25e98c16b8e5b5229fe0e6bb
Joey Forzani <jf4555@princeton.edu>
2024-04-24 18:29:59 -0400

    bug

--- stressTestMaker.c
+++ stressTestMaker.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
+int main() {
     int i;
     for(i = 0; i < 48000; i++) {
         int randnum = (int) ((double)rand() * 97.0 / 
@@ -10,4 +10,5 @@ void main() {
         else if (randnum == 128) randnum = 10;
         putchar((char) randnum);
     }
+    return 0;
 }
