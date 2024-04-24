b6b7c6af4c3830a508567d3fa64f726e7f12ea43
Joey Forzani <jf4555@princeton.edu>
2024-04-24 18:36:42 -0400

    fixed seeding on stresstestmaker

--- stressTestMaker.c
+++ stressTestMaker.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
     int i;
     for(i = 0; i < 48000; i++) {
-        int randnum = (int) ((double)rand() * 97.0 / 
-                    (double)((unsigned)RAND_MAX + 1) + 32);
+        int randnum = (int) ((double)srand(time(NULL)) % 97.0 + 32.0);
         if (randnum == 127) randnum = 9;
         else if (randnum == 128) randnum = 10;
         putchar((char) randnum);
