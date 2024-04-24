7ca4dc891f3a255993f20570f98fc7be71b164af
Joey Forzani <jf4555@princeton.edu>
2024-04-24 18:42:45 -0400

    forgor semicolon

--- stressTestMaker.c
+++ stressTestMaker.c
@@ -5,7 +5,7 @@
 int main() {
     int i;
     for(i = 0; i < 48000; i++) {
-        srand(time(NULL))
+        srand(time(NULL));
         int randnum = rand() % 97.0 + 32.0;
         if (randnum == 127) randnum = 9;
         else if (randnum == 128) randnum = 10;
