c3618b7a82373af24afd6efb17925578f3153b78
Joey Forzani <jf4555@princeton.edu>
2024-04-23 23:03:36 -0400

    guarded loop

--- bigintaddoptopt.s
+++ bigintaddoptopt.s
@@ -18,58 +18,6 @@
 
         .section .text
 
-        //--------------------------------------------------------------
-        // Return the larger of lLength1 and lLength2.
-        //--------------------------------------------------------------
-
-        // Must be a multiple of 16
-        .equ    LARGER_STACK_BYTECOUNT, 32
-
-        // Offsets for local variables
-        .equ    lLength1, 8
-        .equ    lLength2, 16
-        .equ    lLarger, 24
-        LLENGTH1        .req x19
-        LLENGTH2        .req x20
-        LLARGER         .req x21
-
-
-BigInt_larger:
-        // Prolog
-        sub     sp, sp, LARGER_STACK_BYTECOUNT
-        str     x30, [sp]
-        str     x19, [sp, lLength1]
-        str     x20, [sp, lLength2]
-        str     x21, [sp, lLarger]
-        mov     LLENGTH1, x0
-        mov     LLENGTH2, x1
-        
-
-        // if (lLength1 <= lLength2) goto Else1
-        cmp     LLENGTH1, LLENGTH2
-        ble     Else1
-
-        // lLarger = lLength1
-        mov     LLARGER, LLENGTH1
-
-        // goto endIf1
-        b       endIf1
-
-Else1:
-        // lLarger = lLength2
-        mov     LLARGER, LLENGTH2
-
-endIf1:
-        // Epilogue and return lLarger
-        mov     x0, LLARGER
-        ldr     LLENGTH1, [sp, lLength1]
-        ldr     LLENGTH2, [sp, lLength2]
-        ldr     LLARGER, [sp, lLarger]
-        ldr     x30, [sp]
-        add     sp, sp, LARGER_STACK_BYTECOUNT
-        ret
-
-        .size   BigInt_larger, (. - BigInt_larger)
 
         //--------------------------------------------------------------
         // Assign the sum of oAddend1 and oAddend2 to oSum. oSum should 
@@ -151,10 +99,6 @@ endIf2:
         mov     LINDEX, 0
 
 startForLoop1:
-        // if(lIndex >= lSumLength) goto endForLoop1
-        cmp     LINDEX, LSUMLENGTH
-        bge     endForLoop1
-
         // ulSum = ulCarry
         mov     ULSUM, ULCARRY
 
@@ -199,10 +143,10 @@ ForIf1:
         // lIndex++
         add     LINDEX, LINDEX, 1
 
-        // goto startForLoop1
-        b       startForLoop1
+        // if(lIndex < lSumLength) goto startForLoop1
+        cmp     LINDEX, LSUMLENGTH
+        blt     startForLoop1
 
-endForLoop1:
         // if (ulCarry != 1) goto endIf3
         mov     x0, 1
         cmp     ULCARRY, x0
